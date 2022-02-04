fad0ea165d8f78a25ff7c0aebed1aa8e0c193cdc
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2022-02-05 00:37:19 +0600

    updated and implemented multiple states to one state and passed value to parent.

--- src/components/UI/Forms/ExpenseForm/ExpenseForm.js
+++ src/components/UI/Forms/ExpenseForm/ExpenseForm.js
@@ -1,19 +1,76 @@
 import './ExpenseForm.css'
+import {useState} from "react";
 
 const ExpenseForm = (props) => {
-    return (<form>
+    // Multiple State Example
+    // const [titleInput, setTitleInput] = useState('');
+    // const [amountInput, setAmountInput] = useState(0);
+    // const [dateInput, setDateInput] = useState(new Date().toString());
+
+    const [expense, setExpense] = useState({
+        title: '', amount: 0, date: ''
+    })
+    console.log(expense.date);
+    const titleChangeHandler = (event) => {
+        // it's recommended to use inner arrow function for updating state that depends on previous state.
+        setExpense((prevState) => {
+            return {
+                ...prevState, title: event.target.value
+            }
+        })
+        // setExpenseInput({
+        //     ...expenseInput,
+        //     titleInput: event.target.value
+        // })
+    }
+    const amountChangeHandler = (event) => {
+        setExpense((prevState) => {
+            return {
+                ...prevState, amount: event.target.value
+            }
+        })
+    }
+    const dateChangeHandler = (event) => {
+        setExpense((prevState) => {
+            return {
+                ...expense, date: new Date(event.target.value)
+            }
+
+        })
+    }
+    const onSubmitFormHandler = (event) => {
+        event.preventDefault()
+        props.onSubmitExpenseData(expense);
+        setExpense({
+            title: '',
+            amount: 0,
+            date: ''
+        })
+    }
+    return (<form onSubmit={onSubmitFormHandler}>
         <div className={'new-expense__controls'}>
             <div className={'new-expense__control'}>
                 <label>Title</label>
-                <input type="text"/>
+                <input type="text"
+                       value={expense.title}
+                       placeholder={'Enter expense title'}
+                       onChange={titleChangeHandler}/>
             </div>
             <div className={'new-expense__control'}>
                 <label>Amount</label>
-                <input type="number" min="5" step="5"/>
+                <input type="number"
+                       min="5"
+                       step="5"
+                       placeholder={'Enter expense amount'}
+                       onChange={amountChangeHandler}/>
             </div>
             <div className={'new-expense__control'}>
                 <label>Date</label>
-                <input type="date" min="2022-01-01"max="2025-12-31"/>
+                <input type="date"
+                       min="2022-01-01"
+                       max="2025-12-31"
+                       // value={expense.date}
+                       onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className={'new-expense__actions'}>
