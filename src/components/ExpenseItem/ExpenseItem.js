b4a14445c5aa1cc5aea4f7d5ae96aaab7864db55
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2022-02-04 07:51:22 +0600

    introducing react state on expenseItem

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseItem.css'
 import ExpanseDate from "../ExpanseDate/ExpanseDate";
 import Card from "../UI/Card/Card";
@@ -6,12 +6,11 @@ import Card from "../UI/Card/Card";
 const ExpenseItem = (props) => {
 
     const onEditButtonClicked = () => {
-        this.title = 'Mobile Purchase';
-        console.log(this.title);
+        setTitle('Mobile Purchase');
     }
-    let date = props.expense.date;
-    let title = props.expense.title;
-    let amount = props.expense.amount;
+    let [date, setDate] = useState(props.expense.date);
+    let [title, setTitle] = useState(props.expense.title);
+    let [amount, setAmount] = useState(props.expense.amount);
 
     return (<Card className={'expense-item'}>
         <ExpanseDate date={date}/>
